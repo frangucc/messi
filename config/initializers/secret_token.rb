c2ba759cef249a87c4eba35d868fc74814fd0416
Edward Anderson <ejanderson4@gmail.com>
2013-05-31 11:35:22 -0500

    adding rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Messi::Application.config.secret_token = '08ae5bb7ac1eeb35f558f813c147c7ce8cd40cc7afa67b81cf8ebc324d6f93d081dadea120a71e822e9179dbcc2472fa2a82890b1f8f8a23476184bf272d6498'
